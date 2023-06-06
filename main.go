1b057997b7fd7a60370b4f3cefb77191a09810ca
Jesse <jessebot.git@gmail.com>
2023-06-05 18:29:08 -0700

    start reading configuration contents

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
+
+type Config struct {
+	NoteDirectory string `json:"note_directory"`
+	Editor        string `json:"editor"`
+}
+
+var NoteConfigDirectoryEnvVar string = "NOTE_DIRECTORY"
+var DefaultConfigContents []byte = []byte(`{
+	"note_directory": "~/Documents/notes",
+	"editor": "Notepad.exe"
+}`)
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Error: invalid command. See command `help` for more information")
+		os.Exit(1)
+	}
+
+	err := readFromConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
 
@@ -33,3 +51,46 @@ func main() {
 	}
 }
 
+func readFromConfig() error {
+	var configFilePath string
+	if os.Getenv(NoteConfigDirectoryEnvVar) == "" {
+		xdgConfigHome, err := os.UserConfigDir()
+		if err != nil {
+			return err
+		}
+
+		configFilePath = filepath.Join(xdgConfigHome, "note", "config.json")
+	} else {
+		configFilePath = os.Getenv(NoteConfigDirectoryEnvVar)
+	}
+
+	configFileContents, err := os.ReadFile(configFilePath)
+	if err != nil {
+		configFileContents = DefaultConfigContents
+
+		err := writeDefaultConfig(configFilePath)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Print(string(configFileContents))
+
+	return nil
+}
+
+func writeDefaultConfig(configFilePath string) (error) {
+	configPathDir := filepath.Dir(configFilePath)
+
+	err := os.MkdirAll(configPathDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	writeErr := os.WriteFile(configFilePath, DefaultConfigContents, 0644)
+	if writeErr != nil {
+		return err
+	}
+
+	return nil
+}
